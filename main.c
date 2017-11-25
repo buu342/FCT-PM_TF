9aef99733013463757b4504b56151daf8f6e44be
buu342 <buu342@hotmail.com>
2017-11-25 20:26:15 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+/*--------------------------------------------------------------
+                    Standard C Libraries
+--------------------------------------------------------------*/
+
+#include <stdio.h> // For use of literally everything (Thanks Mike Lesk!)
+#include <stdlib.h> // For use of the exit and system functions
+#include <stdio_ext.h> // For use of the __fpurge function
+#include <string.h> // For use of the string library
+
+
+/*--------------------------------------------------------------
+                        Custom Libraries
+--------------------------------------------------------------*/
+
+#include <menu.h> // Menu
+
+
+/*--------------------------------------------------------------
+                            Main
+--------------------------------------------------------------*/
 
 int main()
 {
-	printf("Hello World\n");
-	printf("pls give 20");
+    menu();
 }
