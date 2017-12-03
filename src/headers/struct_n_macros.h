6b46d0dacb75400ff6bd90186ab96ffe6cf7e919
Buu342 <buu342@hotmail.com>
2017-12-03 23:05:43 +0000

    Initial commit

---
+++ src/headers/struct_n_macros.h
@@ -0,0 +1,21 @@
+#ifndef PMPASN_H_
+#define PMPASN_H_
+
+#define cr_red     "\x1b[31m"
+#define cr_green   "\x1b[32m"
+#define cr_yellow  "\x1b[33m"
+#define cr_blue    "\x1b[34m"
+#define cr_magenta "\x1b[35m"
+#define cr_cyan    "\x1b[36m"
+#define cr_reset   "\x1b[0m"
+
+typedef struct Passenger
+{
+    unsigned short int id;
+    char name[51];
+    char orig[11];
+    char dest[11];
+    unsigned short int day;
+} PASSENGER;
+
+#endif
