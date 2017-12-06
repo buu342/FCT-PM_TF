6bad2b8fcfa8ba25583db73d8619a1df82029a4e
Buu342 <buu342@hotmail.com>
2017-12-06 00:52:51 +0000

    Moved the asterisk (VERY IMPORTANT)

--- src/headers/store.h
+++ src/headers/store.h
@@ -1,8 +1,8 @@
 #ifndef PMSTOR_H_
 #define PMSTOR_H_
 
-int store_text(PASSENGER* passengers, char *filename);
+int store_text(PASSENGER *passengers, char *filename);
 
-int store_binary(PASSENGER* passengers, char *filename);
+int store_binary(PASSENGER *passengers, char *filename);
 
 #endif
