bbd47d05fba1c5b6d50beb4410330fa516114d25
Buu342 <buu342@hotmail.com>
2017-12-06 00:50:19 +0000

    Redid the function, works like the teacher asked this time

--- src/headers/list.h
+++ src/headers/list.h
@@ -1,6 +1,8 @@
 #ifndef PMLIST_H_
 #define PMLIST_H_
 
-void list_passengers(PASSENGER* passengers);
+int list_passengers(PASSENGER *passengers, int day_check, int flight_check);
+
+void ten_day_table(PASSENGER *passengers);
 
 #endif
