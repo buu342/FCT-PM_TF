f3d37084d2e7e4f4046ca19c592b8e3d85655f8b
Buu342 <buu342@hotmail.com>
2017-12-07 15:55:00 +0000

    Removed extra _

--- src/headers/store.h
+++ src/headers/store.h
@@ -1,8 +1,8 @@
-#ifndef PMSTOR_H_
-#define PMSTOR_H_
+#ifndef PMSTOR_H
+#define PMSTOR_H
 
-int store_text(PASSENGER *passengers, char *filename);
+int store_text(PASSENGER *passengers, char *argv);
 
-int store_binary(PASSENGER *passengers, char *filename);
+int store_binary(PASSENGER *passengers, char *argv);
 
 #endif
