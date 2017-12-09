ab4163d61768c7d435c2710d39129a2f9bff3ae0
Buu342 <buu342@hotmail.com>
2017-12-09 02:31:38 +0000

    Initial commit

---
+++ src/reserve.c
@@ -0,0 +1,27 @@
+/*--------------------------------------------------------------
+                    Standard C Libraries
+--------------------------------------------------------------*/
+
+#include <stdio.h>      // For use of literally everything (Thanks Mike Lesk!)
+#include <sys/stat.h>   // For use of the stat function
+#include <unistd.h>     // For use of the mkdir function
+#include <sys/types.h>  // For use of ACCESSPERMS
+#include <string.h>     // For use of the strcat function
+
+/*--------------------------------------------------------------
+                Custom Libraries and macros
+--------------------------------------------------------------*/
+
+#include "headers/struct_n_macros.h"
+#include "headers/reserve.h"
+
+
+/*--------------------------------------------------------------
+                       write_textfile
+            Writes the database to a text file.
+--------------------------------------------------------------*/
+
+void reserve(PASSENGER *passengers, int mode)
+{
+
+}
