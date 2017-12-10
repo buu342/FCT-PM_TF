552f5f7c6a76d530339cb6edad8bf28fbb956b23
Buu342 <buu342@hotmail.com>
2017-12-10 20:25:01 +0000

    Finalized

---
+++ src/output.c
@@ -0,0 +1,49 @@
+/*--------------------------------------------------------------
+                    Standard C Libraries
+--------------------------------------------------------------*/
+
+#include <stdio.h>      // For use of everything
+#include <sys/stat.h>   // For use of the stat function
+#include <unistd.h>     // For use of the mkdir function
+#include <sys/types.h>  // For use of ACCESSPERMS
+#include <string.h>     // For use of the strcat function
+
+
+/*--------------------------------------------------------------
+                Custom Libraries and macros
+--------------------------------------------------------------*/
+
+#include "headers/struct_n_macros.h"
+#include "headers/output.h"
+
+
+/*--------------------------------------------------------------
+                       write_textfile
+            Exports the database to a text file.
+--------------------------------------------------------------*/
+
+void write_file(PASSENGER *passengers)
+{
+    FILE *p_file;
+    int i=0;
+    char name[140]; // Now you can name your text file after your tweet!
+    char filename[146] = "texts/"; // The location it will be saved to.
+    struct stat struc = {0}; // Needed for the stat function. Won't be used.
+
+    printf(cr_magenta "Indica o nome do ficheiro para guardar: " cr_reset); // Request file name.
+    scanf(" %s", name);
+
+    strcat(filename,name); // Merge the words texts/ and the filename together
+
+    if (stat("/texts", &struc) == -1) // Check if the directory exists
+        mkdir("texts", ACCESSPERMS); // if not make it. Permissions to everyone.
+
+    p_file = fopen(filename, "w+");
+
+    for (i=0;passengers[i].id!=0;i++) // Write until no more information.
+        fprintf(p_file, "%d%s %-10s %-10s %2d\n", passengers[i].id, passengers[i].name, passengers[i].orig, passengers[i].dest, passengers[i].day );
+
+    fclose(p_file);
+
+    printf(cr_yellow "Ficheiro guardado como %s.\n\n" cr_reset, filename);
+}
